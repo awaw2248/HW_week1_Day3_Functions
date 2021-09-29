a09e8d75663495fa791357ddb2084388ed6bace5
Ahmed Al Hassan <aaa.11.26@outlook.com>
2021-09-29 12:41:56 +0300

    hw2

---
+++ hw2/src/main/kotlin/solution_part1.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    val firstNum = 3
+    val secondNum = 20
+
+    val sum = firstNum + secondNum
+
+    println("sum = $sum")
+
+fun fullName(firstName: String, lastName: String) = "$firstName $lastName"
+
+
+    println(fullName("Ahmed", "Al Hassan"))
+    println(fullName("Ahmed", "Abdullah"))
+}
